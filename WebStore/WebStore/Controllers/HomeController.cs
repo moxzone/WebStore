75ebc2704dc3fac51adad9c0dc663e92446a4566
moxzone <moxzone@hotmail.com>
2017-09-08 00:34:07 +0800

    1

--- WebStore/WebStore/Controllers/HomeController.cs
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace WebStore.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-             
+            object o = new Object();
             return View();
         }
     }

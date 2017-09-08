332479052dff496e814f462ef48b88539e8e6273
moxzone <moxzone@hotmail.com>
2017-09-08 11:15:34 +0800

    11

--- WebStore/WebStore/App_Start/RouteConfig.cs
+++ WebStore/WebStore/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace WebStore
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Test",
+                url: "{controller}/{action}/{name}/{id}"
+                );
         }
     }
 }
